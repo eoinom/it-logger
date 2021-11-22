879800fba7d2f2f94cce86831a554e3f8943a15a
Eoin O'Malley <eomalley@letsgetchecked.com>
2021-11-22 18:20:38 +0000

    Lesson 79 (AddBtn & AddLogModal Components)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Fragment, useEffect } from 'react';
 import SearchBar from './components/layout/SearchBar';
 import Logs from './components/logs/Logs';
+import AddBtn from './components/layout/AddBtn';
+import AddLogModal from './components/logs/AddLogModal';
 
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
@@ -10,16 +12,18 @@ const App = () => {
   useEffect(() => {
     // initialise Materialize JS
     M.AutoInit();
-  })
+  });
 
   return (
     <Fragment>
       <SearchBar />
-      <div className="container">
+      <div className='container'>
+        <AddBtn />
+        <AddLogModal />
         <Logs />
       </div>
     </Fragment>
   );
-}
+};
 
 export default App;
